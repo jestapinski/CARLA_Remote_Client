119f9b694e084184c9de6a3f1d9384c8fbb7f0a6
Jordan Stapinski <jstapins@andrew.cmu.edu>
2018-06-25 13:31:09 -0400

    Added sompleted implementation of basic cardinal directions and takeover with FINAL IP

--- use_case_controller.py
+++ use_case_controller.py
@@ -15,6 +15,7 @@ import argparse
 import logging
 import random
 import time
+import _thread
 
 from carla import image_converter
 from carla import sensor
@@ -25,6 +26,14 @@ from carla.tcp import TCPConnectionError
 from carla.util import print_over_same_line
 
 game = None
+# Default '' refers to '0.0.0.0', or this machine's public IP
+THIS_IP = ''
+THIS_PORT = 8080
+REMOTE_IP = '172.25.2.19'
+REMOTE_PORT = 2000
+LATENCY_MEASURE = 1000 # Number of pings to get the message across
+QUALITY_LEVEL = 'Epic'
+MAP_NAME = 'Town01'
 
 def make_carla_settings(args):
     """Make a CarlaSettings object with the settings we need."""
@@ -35,41 +44,43 @@ def make_carla_settings(args):
         NumberOfVehicles=15,
         NumberOfPedestrians=30,
         WeatherId=random.choice([1, 3, 7, 8, 14]),
-        QualityLevel=args.quality_level)
+        QualityLevel=QUALITY_LEVEL)
     return settings
 
 
-class Timer(object):
-    def __init__(self):
-        self.step = 0
-        self._lap_step = 0
-        self._lap_time = time.time()
+# class Timer(object):
+#     def __init__(self):
+#         self.step = 0
+#         self._lap_step = 0
+#         self._lap_time = time.time()
 
-    def tick(self):
-        self.step += 1
+#     def tick(self):
+#         self.step += 1
 
-    def lap(self):
-        self._lap_step = self.step
-        self._lap_time = time.time()
+#     def lap(self):
+#         self._lap_step = self.step
+#         self._lap_time = time.time()
 
-    def ticks_per_second(self):
-        return float(self.step - self._lap_step) / self.elapsed_seconds_since_lap()
+#     def ticks_per_second(self):
+#         return float(self.step - self._lap_step) / self.elapsed_seconds_since_lap()
 
-    def elapsed_seconds_since_lap(self):
-        return time.time() - self._lap_time
+#     def elapsed_seconds_since_lap(self):
+#         return time.time() - self._lap_time
 
 
 class CarlaGame(object):
     def __init__(self, carla_client, args):
         self.client = carla_client
         self._carla_settings = make_carla_settings(args)
-        # self._timer = None
-        self._enable_autopilot = args.autopilot
+        # self._timer = Timer()
+        self._enable_autopilot = True
         self._is_on_reverse = False
-        self._city_name = args.map_name
+        self._city_name = MAP_NAME
         self._position = None
         self._agent_positions = None
+        self._map = CarlaMap(self._city_name, 0.1643, 50.0) if self._city_name is not None else None
         self.measurements = None
+        self.enabled_commands = []
 
     def execute(self):
         """Launch the PyGame."""
@@ -77,62 +88,88 @@ class CarlaGame(object):
         try:
             while True:
                 self._on_loop()
-        except BaseException as e:
+        except Exception as e:
             print(e.message, e.args)
 
     def _initialize_game(self):
         self._on_new_episode()
 
+# Callback function to be run whenever the game restarts
+
     def _on_new_episode(self):
         scene = self.client.load_settings(self._carla_settings)
         number_of_player_starts = len(scene.player_start_spots)
-        player_start = np.random.randint(number_of_player_starts)
+        # Same default start spot
+        player_start = 1
         print('Starting new episode...')
         self.client.start_episode(player_start)
         # self._timer = Timer()
         self._is_on_reverse = False
+        self.enabled_commands = []
+
+# Callback function to be run continuously throughout the game
 
     def _on_loop(self):
-        self._timer.tick()
+        # self._timer.tick()
 
         self.measurements, sensor_data = self.client.read_data()
 
-        # Print measurements every second.
-        # if self._timer.elapsed_seconds_since_lap() > 1.0:
-            # Plot position on the map as well.
-
-            # self._timer.lap()
-
         # Set the player position
         if self._city_name is not None:
+            measurements = self.measurements
             self._position = self._map.convert_to_pixel([
                 measurements.player_measurements.transform.location.x,
                 measurements.player_measurements.transform.location.y,
                 measurements.player_measurements.transform.location.z])
             self._agent_positions = measurements.non_player_agents
 
+        number_of_commands = len(self.enabled_commands)
+        if self._enable_autopilot or number_of_commands == 0:
+            self.client.send_control(self.measurements.player_measurements.autopilot_control)
+        # else:
+        if (number_of_commands > 0):
+            for command in self.enabled_commands:
+                command()
 
+# Interface Functions
+# The following functions control the car based on the POST payload
+
+# Autopilot
     def trigger_ap(self):
         self._enable_autopilot = not self._enable_autopilot
-        self.client.send_control(self.measurements.player_measurements.autopilot_control)
+        self.clear()
 
+# Reset
     def trigger_reset(self):
         self._on_new_episode()
 
+# Left turning
     def steer_left(self):
         control = VehicleControl()
-        control.steer = -1.0
+        control.steer = min(control.steer - 0.001, -1.0)
         control.reverse = self._is_on_reverse
         self.client.send_control(control)
 
+    def add_left(self):
+        self.enabled_commands.append(self.steer_left)
+
+# Right Turning
     def steer_right(self):
         control = VehicleControl()
-        control.steer = 1.0
+        control.steer = max(control.steer + 0.001, 1.0)
         control.reverse = self._is_on_reverse
         self.client.send_control(control)
 
+    def add_right(self):
+        self.enabled_commands.append(self.steer_right)
+
+# Reverse
     def trigger_reverse(self):
         self._is_on_reverse = not self._is_on_reverse
+
+# Acceleration
+    def add_accel(self):
+        self.enabled_commands.append(self.accelerate)
 
     def accelerate(self):
         control = VehicleControl()
@@ -140,20 +177,30 @@ class CarlaGame(object):
         control.reverse = self._is_on_reverse
         self.client.send_control(control) 
 
+# Deceleration
+    def add_decel(self):
+        self.enabled_commands.append(self.decelerate)
+
     def decelerate(self):
         control = VehicleControl()
         control.brake = 1.0
         control.reverse = self._is_on_reverse
         self.client.send_control(control)
 
+# Braking
+    def addBrake(self):
+        self.enabled_commands = [self.throw_brake]
+
     def throw_brake(self):
         control = VehicleControl()
         control.hand_brake = True
-        self.client.send_control(control)            
+        self.client.send_control(control) 
 
- 
+# Clearing all commands
+    def clear(self):
+        self.enabled_commands = []
 
-def main():
+def main(obj):
     argparser = argparse.ArgumentParser(
         description='CARLA Manual Control Client')
     argparser.add_argument(
@@ -164,26 +211,14 @@ def main():
     argparser.add_argument(
         '--host',
         metavar='H',
-        default='128.2.100.39',
+        default=REMOTE_IP,
         help='IP of the host server (default: localhost)')
     argparser.add_argument(
         '-p', '--port',
         metavar='P',
-        default=2000,
+        default=REMOTE_PORT,
         type=int,
         help='TCP port to listen to (default: 2000)')
-    argparser.add_argument(
-        '-q', '--quality-level',
-        choices=['Low', 'Epic'],
-        type=lambda s: s.title(),
-        default='Epic',
-        help='graphics quality level, a lower level makes the simulation run considerably faster.')
-    argparser.add_argument(
-        '-m', '--map-name',
-        metavar='M',
-        default='Town01',
-        help='plot the map of the current city (needs to match active map in '
-             'server, options: Town01 or Town02)')
     args = argparser.parse_args()
 
     log_level = logging.DEBUG if args.debug else logging.INFO
@@ -197,13 +232,16 @@ def main():
         try:
             # Construct server here
             # When response then start client
+            print("trying server", args.host, args.port, make_carla_client(args.host, args.port))
             with make_carla_client(args.host, args.port) as client:
                 global game
                 game = CarlaGame(client, args)
+                print("execute")
                 game.execute()
                 break
 
         except TCPConnectionError as error:
+            print("error")
             logging.error(error)
             time.sleep(1)
 
@@ -213,6 +251,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
  
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+
  
   # GET
     def do_GET(self):
@@ -225,7 +266,7 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Send message back to client
-        message = "Hello world!"
+        message = "Connection Successful!"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
@@ -236,34 +277,45 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
         content = post_data.decode("utf-8") 
         print(content)
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send message back to client to help in completing transaction
+        message = "POST!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
         if content == 'testmsg':
             print("testing connection successful")
         if content == 'startserver':
-            main()
-        if content == 'enable_ap':
+            # Dispatch a thread to start a new game instance and save globally
+            _thread.start_new_thread(main, (self,))
+        if content == 'enable_ap': #DONE
             game.trigger_ap()
-        if content == 'reset':
+        if content == 'reset': #BROKEN
             game.trigger_reset()
-        if content == 'left':
-            game.steer_left()
-        if content == 'right':
-            game.steer_right()
-        if content == 'reverse':
+        if content == 'left': #DONE
+            game.add_left()
+        if content == 'right': #DONE
+            game.add_right()
+        if content == 'reverse': #DONE
             game.trigger_reverse()
         if content == 'forward':
-            game.accelerate()
+            game.add_accel()
         if content == 'backward':
-            game.decelerate()
+            game.add_decel()
         if content == 'stop':
-            game.throw_brake()
-
+            game.add_brake()
+        # ADD CLEAR
+        if content == 'clear':
+            game.clear()
+        print("sending Response")
 
 def run():
   print('starting server...')
  
   # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('', 8080)
+  server_address = (THIS_IP, THIS_PORT)
+  # print(make_carcla_client(REMOTE_IP, REMOTE_PORT))
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...', httpd.socket.getsockname())
   print(httpd)
