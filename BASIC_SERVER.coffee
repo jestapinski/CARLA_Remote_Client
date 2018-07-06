2c91344d50a2336ebbc96162cc82d3489509314e
Jordan Stapinski <jstapins@andrew.cmu.edu>
2018-07-06 16:39:14 -0400

    Constructed Framer module for Secondary Screen

---
+++ BASIC_SERVER.coffee
@@ -0,0 +1,54 @@
+###
+This file outlines the basic secondary screen server as part of the
+interactive Framer.JS prototype for the MHCI Harman Capstone at 
+Carnegie Mellon University, 2018.
+
+This is a Framer Module to be used as a basic protocol to be implemented
+
+Functions to be implemented in base Framer project:
+runReset()
+beginShiftingLeft()
+stopShiftingLeft()
+startShiftingRight()
+stopShiftingRight()
+startSpeedingUp()
+stopSpeedingUp()
+startSlowingDown()
+stopSlowingDown()
+stop()
+###
+
+PORT_NUM = 8020
+OK_RESPONSE = 200
+
+
+exports.start_secondary_screen_server = ->
+  server = http.createServer (req, res) ->
+    console.log res.url
+
+    content = req.url[1..] # Getting the passed command
+
+    # Case on content
+    switch (content){
+      when 'reset' then runReset()
+      when 'left' then beginShiftingLeft()
+      when 'left_stop' then stopShiftingLeft()
+      when 'right' then startShiftingRight()
+      when 'right_stop' then stopShiftingRight()
+      when 'speed_up' then startSpeedingUp()
+      when 'speed_up_stop' then stopSpeedingUp()
+      when 'slow_down' then startSlowingDown()
+      when 'slow_down_stop' then stopSlowingDown()
+      when 'stop' then stop()
+    }
+    if content === 'left':
+      beginShiftingLeft()
+    if content
+
+    data = 'Got Your Message\n'
+    res.writeHead OK_RESPONSE,
+        'Content-Type':     'text/plain'
+        'Content-Length':   data.length
+    res.end data
+
+  server.listen PORT_NUM
--- use_case_controller.py
+++ use_case_controller.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
+# Based off of Keyboard Input Framework from UAB
 # Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de
 # Barcelona (UAB).
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
 
-# Keyboard controlling for CARLA. Please refer to client_example.py for a simpler
-# and more documented example.
+# Extension by Jordan Stapinski, to allow incoming HTTP connections to provide
+# remote control to the vehicle, allowing for cross-language control from
+# remote devices, implementing a customized protocol
 
 from __future__ import print_function
 
@@ -16,6 +15,7 @@ import logging
 import random
 import time
 import _thread
+import requests
 
 from carla import image_converter
 from carla import sensor
@@ -25,179 +25,204 @@ from carla.settings import CarlaSettings
 from carla.tcp import TCPConnectionError
 from carla.util import print_over_same_line
 
+# global game instance to be used
 game = None
+
 # Default '' refers to '0.0.0.0', or this machine's public IP
 THIS_IP = ''
 THIS_PORT = 8080
+
+# Target CARLA Machine IP
 REMOTE_IP = '172.25.2.19'
 REMOTE_PORT = 2000
 LATENCY_MEASURE = 1000 # Number of pings to get the message across
 QUALITY_LEVEL = 'Epic'
 MAP_NAME = 'Town01'
 
+# Target Framer Secondary Screen IP
+SECONDARY_IP = ''
+SECONDARY_PORT = 8079
+SECONDARY_SCREEN_COMMANDS = {
+  'reset': 'reset',
+  'left': 'left',
+  'left_stop': 'left_stop',
+  'right': 'right',
+  'right_stop': 'right_stop',
+  'speed_up': 'speed_up',
+  'speed_up_stop': 'speed_up_stop',
+  'slow_down': 'slow_down',
+  'slow_down_stop': 'slow_down_stop',
+  'stop': 'stop'
+}
+
 def make_carla_settings(args):
-    """Make a CarlaSettings object with the settings we need."""
-    settings = CarlaSettings()
-    settings.set(
-        SynchronousMode=False,
-        SendNonPlayerAgentsInfo=True,
-        NumberOfVehicles=15,
-        NumberOfPedestrians=30,
-        WeatherId=random.choice([1, 3, 7, 8, 14]),
-        QualityLevel=QUALITY_LEVEL)
-    return settings
+  """Make a CarlaSettings object with the settings we need."""
+  settings = CarlaSettings()
+  settings.set(
+      SynchronousMode=False,
+      SendNonPlayerAgentsInfo=True,
+      NumberOfVehicles=15,
+      NumberOfPedestrians=30,
+      WeatherId=random.choice([1, 3, 7, 8, 14]),
+      QualityLevel=QUALITY_LEVEL)
+  return settings
 
 class CarlaGame(object):
-    def __init__(self, carla_client, args):
-        self.client = carla_client
-        self._carla_settings = make_carla_settings(args)
-        # self._timer = Timer()
-        self._enable_autopilot = True
-        self._is_on_reverse = False
-        self._city_name = MAP_NAME
-        self._position = None
-        self._agent_positions = None
-        self._map = CarlaMap(self._city_name, 0.1643, 50.0) if self._city_name is not None else None
-        self.measurements = None
-        self.enabled_commands = []
+  def __init__(self, carla_client, args):
+    self.client = carla_client
+    self._carla_settings = make_carla_settings(args)
+    self._enable_autopilot = True
+    self._is_on_reverse = False
+    self._city_name = MAP_NAME
+    self._position = None
+    self._agent_positions = None
+    self._map = CarlaMap(self._city_name, 0.1643, 50.0) if self._city_name is not None else None
+    self.measurements = None
+    self.enabled_commands = []
 
-    def execute(self):
-        """Launch the PyGame."""
-        self._initialize_game()
-        try:
-            while True:
-                self._on_loop()
-        except Exception as e:
-            print(e.message, e.args)
+  def execute(self):
+    self._initialize_game()
+    try:
+      while True:
+        self._on_loop()
+    except Exception as e:
+      print(e.message, e.args)
 
-    def _initialize_game(self):
-        self._on_new_episode()
+  def _initialize_game(self):
+    self._on_new_episode()
 
 # Callback function to be run whenever the game restarts
 
-    def _on_new_episode(self):
-        scene = self.client.load_settings(self._carla_settings)
-        number_of_player_starts = len(scene.player_start_spots)
-        # Same default start spot
-        player_start = 1
-        print('Starting new episode...')
-        self.client.start_episode(player_start)
-        # self._timer = Timer()
-        self._is_on_reverse = False
-        self.enabled_commands = []
+  def _on_new_episode(self):
+    scene = self.client.load_settings(self._carla_settings)
+    number_of_player_starts = len(scene.player_start_spots)
+    # Same default start spot
+    player_start = 1
+    print('Starting new episode...')
+    self.client.start_episode(player_start)
+    self._is_on_reverse = False
+    self.enabled_commands = []
 
 # Callback function to be run continuously throughout the game
 
-    def _on_loop(self):
-        # self._timer.tick()
+  def _on_loop(self):
+    self.measurements, sensor_data = self.client.read_data()
 
-        self.measurements, sensor_data = self.client.read_data()
+    # Set the player position
+    if self._city_name is not None:
+      measurements = self.measurements
+      self._position = self._map.convert_to_pixel([
+        measurements.player_measurements.transform.location.x,
+        measurements.player_measurements.transform.location.y,
+        measurements.player_measurements.transform.location.z])
+      self._agent_positions = measurements.non_player_agents
 
-        # Set the player position
-        if self._city_name is not None:
-            measurements = self.measurements
-            self._position = self._map.convert_to_pixel([
-                measurements.player_measurements.transform.location.x,
-                measurements.player_measurements.transform.location.y,
-                measurements.player_measurements.transform.location.z])
-            self._agent_positions = measurements.non_player_agents
-
-        number_of_commands = len(self.enabled_commands)
-        if self._enable_autopilot or number_of_commands == 0:
-            self.client.send_control(self.measurements.player_measurements.autopilot_control)
-        # else:
-        if (number_of_commands > 0):
-            for command in self.enabled_commands:
-                command()
+    number_of_commands = len(self.enabled_commands)
+    if self._enable_autopilot and number_of_commands == 0:
+      self.client.send_control(self.measurements.player_measurements.autopilot_control)
+    # else:
+    if (number_of_commands > 0):
+      for command in self.enabled_commands:
+        command()
 
 # Interface Functions
 # The following functions control the car based on the POST payload
 
 # Autopilot
-    def trigger_ap(self):
-        self._enable_autopilot = not self._enable_autopilot
-        self.clear()
+  def trigger_ap(self):
+    self._enable_autopilot = not self._enable_autopilot
+    self.clear()
 
 # Reset
-    def trigger_reset(self):
-        self._on_new_episode()
+  def trigger_reset(self):
+    self._on_new_episode()
+    send_framer_message(SECONDARY_SCREEN_COMMANDS['reset'])
 
 # Left turning
-    def steer_left(self):
-        control = VehicleControl()
-        control.steer = min(control.steer - 0.001, -1.0)
-        control.reverse = self._is_on_reverse
-        self.client.send_control(control)
+  def steer_left(self):
+    control = VehicleControl()
+    control.steer = min(control.steer - 0.001, -1.0)
+    control.reverse = self._is_on_reverse
+    self.client.send_control(control)
 
-    def add_left(self):
-        if self.steer_left not in self.enabled_commands:
-            self.enabled_commands.append(self.steer_left)
+  def add_left(self):
+    if self.steer_left not in self.enabled_commands:
+      self.enabled_commands.append(self.steer_left)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['left'])
 
-    def remove_left(self):
-        if self.steer_left in self.enabled_commands:
-            self.enabled_commands.remove(self.steer_left)
+  def remove_left(self):
+    if self.steer_left in self.enabled_commands:
+      self.enabled_commands.remove(self.steer_left)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['left_stop'])
 
 # Right Turning
-    def steer_right(self):
-        control = VehicleControl()
-        control.steer = max(control.steer + 0.001, 1.0)
-        control.reverse = self._is_on_reverse
-        self.client.send_control(control)
+  def steer_right(self):
+    control = VehicleControl()
+    control.steer = max(control.steer + 0.001, 1.0)
+    control.reverse = self._is_on_reverse
+    self.client.send_control(control)
 
-    def add_right(self):
-        if self.steer_right not in self.enabled_commands:
-            self.enabled_commands.append(self.steer_right)
+  def add_right(self):
+    if self.steer_right not in self.enabled_commands:
+      self.enabled_commands.append(self.steer_right)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['right'])
 
-    def remove_right(self):
-        if self.steer_right in self.enabled_commands:
-            self.enabled_commands.remove(self.steer_right)
+  def remove_right(self):
+    if self.steer_right in self.enabled_commands:
+      self.enabled_commands.remove(self.steer_right)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['right_stop'])
 
 # Reverse
-    def trigger_reverse(self):
-        self._is_on_reverse = not self._is_on_reverse
+  def trigger_reverse(self):
+    self._is_on_reverse = not self._is_on_reverse
 
 # Acceleration
-    def add_accel(self):
-        if self.accelerate not in self.enabled_commands:
-            self.enabled_commands.append(self.accelerate)
+  def add_accel(self):
+    if self.accelerate not in self.enabled_commands:
+      self.enabled_commands.append(self.accelerate)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['speed_up'])
 
-    def remove_accel(self):
-        if self.accelerate in self.enabled_commands:
-            self.enabled_commands.remove(self.accelerate)
+  def remove_accel(self):
+    if self.accelerate in self.enabled_commands:
+      self.enabled_commands.remove(self.accelerate)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['speed_up_stop'])
 
-    def accelerate(self):
-        control = VehicleControl()
-        control.throttle = 1.0
-        control.reverse = self._is_on_reverse
-        self.client.send_control(control) 
+  def accelerate(self):
+    control = VehicleControl()
+    control.throttle = 1.0
+    control.reverse = self._is_on_reverse
+    self.client.send_control(control) 
 
 # Deceleration
-    def add_decel(self):
-        if self.decelerate not in self.enabled_commands:
-            self.enabled_commands.append(self.decelerate)
+  def add_decel(self):
+    if self.decelerate not in self.enabled_commands:
+      self.enabled_commands.append(self.decelerate)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['slow_down'])
 
-    def remove_decel(self):
-        if self.decelerate in self.enabled_commands:
-            self.enabled_commands.remove(self.decelerate)
+  def remove_decel(self):
+    if self.decelerate in self.enabled_commands:
+      self.enabled_commands.remove(self.decelerate)
+      send_framer_message(SECONDARY_SCREEN_COMMANDS['slow_down_stop'])
 
-    def decelerate(self):
-        control = VehicleControl()
-        control.brake = 1.0
-        control.reverse = self._is_on_reverse
-        self.client.send_control(control)
+  def decelerate(self):
+    control = VehicleControl()
+    control.brake = 1.0
+    control.reverse = self._is_on_reverse
+    self.client.send_control(control)
 
 # Braking
-    def add_brake(self):
-        self.enabled_commands = [self.throw_brake]
+  def add_brake(self):
+    self.enabled_commands = [self.throw_brake]
+    send_framer_message(SECONDARY_SCREEN_COMMANDS['stop'])
 
-    def throw_brake(self):
-        control = VehicleControl()
-        control.hand_brake = True
-        self.client.send_control(control) 
+  def throw_brake(self):
+    control = VehicleControl()
+    control.hand_brake = True
+    self.client.send_control(control) 
 
 # Clearing all commands
-    def clear(self):
-        self.enabled_commands = []
+  def clear(self):
+    self.enabled_commands = []
 
 def main(obj):
     argparser = argparse.ArgumentParser(
@@ -247,89 +272,92 @@ def main(obj):
 #!/usr/bin/env python
  
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+def send_framer_message(msg):
+  r = requests.get(SECONDARY_IP + ':' + SECONDARY_PORT, data={'msg': msg})
+  print(r.status_code, r.reason)
  
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
+      super().__init__(request, client_address, server)
 
+  # Additional Header from Coffeescript AJAX calls
     def do_OPTIONS(self):           
-        self.send_response(200, "ok")       
-        self.send_header('Access-Control-Allow-Origin', '*')                
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
-        self.end_headers() 
+      self.send_response(200, "ok")       
+      self.send_header('Access-Control-Allow-Origin', '*')                
+      self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+      self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+      self.end_headers() 
  
   # GET
     def do_GET(self):
-        # Send response status code
-        print("connection!")
-        self.send_response(200)
+      # Send response status code
+      print("connection!")
+      self.send_response(200)
 
-        # Send headers
-        self.send_header('Content-type','text/html')
-        self.end_headers()
+      # Send headers
+      self.send_header('Content-type','text/html')
+      self.end_headers()
 
-        # Send message back to client
-        message = "Connection Successful!"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
+      # Send message back to client
+      message = "Connection Successful!"
+      # Write content as utf-8 data
+      self.wfile.write(bytes(message, "utf8"))
+      return
 
     def do_POST(self):
-        # Doesn't do anything with posted data
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        content = post_data.decode("utf-8") 
-        print(content)
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        # Send message back to client to help in completing transaction
-        message = "POST!"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        if content == 'testmsg':
-            print("testing connection successful")
-        if content == 'startserver':
-            # Dispatch a thread to start a new game instance and save globally
-            _thread.start_new_thread(main, (self,))
-        if content == 'enable_ap': #DONE
-            game.trigger_ap()
-        if content == 'reset': #BROKEN
-            game.trigger_reset()
-        if content == 'left': #DONE
-            game.add_left()
-        if content == 'left_stop': #DONE
-            game.remove_left()
-        if content == 'right': #DONE
-            game.add_right()
-        if content == 'right_stop': #DONE
-            game.remove_right()
-        if content == 'reverse': #DONE
-            game.trigger_reverse()
-        if content == 'forward':
-            game.add_accel()
-        if content == 'forward_stop':
-            game.remove_accel()
-        if content == 'backward':
-            game.add_decel()
-        if content == 'backward_stop':
-            game.remove_decel()
-        if content == 'stop':
-            game.add_brake()
-        # ADD CLEAR
-        if content == 'clear':
-            game.clear()
-        print("sending Response")
+      content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+      post_data = self.rfile.read(content_length) # <--- Gets the data itself
+      content = post_data.decode("utf-8") 
+      print(content)
+      self.send_response(200)
+      self.send_header('Content-type','text/html')
+      self.send_header('Access-Control-Allow-Origin', '*')
+      self.end_headers()
+      # Send message back to client to help in completing transaction
+      message = "POST!"
+      # Write content as utf-8 data
+      self.wfile.write(bytes(message, "utf8"))
+      if content == 'testmsg':
+          print("testing connection successful")
+      if content == 'startserver':
+          # Dispatch a thread to start a new game instance and save globally
+          _thread.start_new_thread(main, (self,))
+      if content == 'enable_ap': #DONE
+          game.trigger_ap()
+      if content == 'reset': #BROKEN
+          game.trigger_reset()
+      if content == 'left': #DONE
+          game.add_left()
+      if content == 'left_stop': #DONE
+          game.remove_left()
+      if content == 'right': #DONE
+          game.add_right()
+      if content == 'right_stop': #DONE
+          game.remove_right()
+      if content == 'reverse': #DONE
+          game.trigger_reverse()
+      if content == 'forward':
+          game.add_accel()
+      if content == 'forward_stop':
+          game.remove_accel()
+      if content == 'backward':
+          game.add_decel()
+      if content == 'backward_stop':
+          game.remove_decel()
+      if content == 'stop':
+          game.add_brake()
+      # ADD CLEAR
+      if content == 'clear':
+          game.clear()
+      print("sending Response")
 
 def run():
   print('starting server...')
  
   # Server settings
   server_address = (THIS_IP, THIS_PORT)
-  # print(make_carcla_client(REMOTE_IP, REMOTE_PORT))
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...', httpd.socket.getsockname())
   print(httpd)
